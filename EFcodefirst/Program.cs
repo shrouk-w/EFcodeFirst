ffff9334fca50ed6076037efc9b09d419e83799b
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-05-27 11:52:49 +0200

    added initial configuaration

--- EFcodefirst/Program.cs
+++ EFcodefirst/Program.cs
@@ -1,3 +1,7 @@
+using EFcodefirst.Middlewares;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 namespace EFcodefirst;
 
 public class Program
@@ -8,6 +12,35 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+        
+        //registering dependencies
+        /*builder.Services.AddScoped<ITripsService, TripsService>();
+        builder.Services.AddScoped<ITripsRepository, TripsRepository>();
+        builder.Services.AddScoped<IClientsService, ClientsService>();
+        builder.Services.AddScoped<IClientsRepository, ClientsRepository>();*/
+        
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "PrescriptionApi",
+                Description = "api for managing prescriptions",
+                Contact = new OpenApiContact
+                {
+                    Name="Api Support",
+                    Email="apiSupport@gmail.com",
+                    Url = new Uri("https://github.com/apiSupport")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
@@ -20,28 +53,27 @@ public class Program
             app.MapOpenApi();
         }
 
+        app.UseGlobalExceptionHandling();
+        
+        app.UseSwagger();
+        
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "webTripApi");
+            
+            c.DocExpansion(DocExpansion.List);
+            c.DefaultModelExpandDepth(0);
+            c.DisplayRequestDuration();
+            c.EnableFilter();
+            
+        });
+        
+
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+        app.MapControllers();
 
         app.Run();
     }
